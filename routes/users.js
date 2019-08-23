82872402e88bcce235ee080dbdd076982305ded2
Murilo <muriloarrudap@gmail.com>
2019-08-22 21:32:45 -0300

    Add contact route CRUD

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 
 // @route   POST api/users
