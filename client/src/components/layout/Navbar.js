6f4251c36b5710b4b006ce9fe84e563d6c8db8f0
Murilo <muriloarrudap@gmail.com>
2019-08-25 22:26:02 -0300

    React setup

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <i className={icon}></i>
+      </h1>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string
+};
+
+Navbar.defaultProps = {
+  title: 'Contact Keeper',
+  icon: 'fas fa-id-card-alt'
+};
+export default Navbar;
