9cdd0d7cef5a1e3d7280658791846c0cd3d1f077
Murilo <muriloarrudap@gmail.com>
2019-09-02 22:08:30 -0300

    Setup Alerts

---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,41 @@
+import React, { useReducer } from 'react';
+
+import AuthContext from './authContext';
+import authReducer from './authReducer';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  LOGOUT,
+  CLEAR_ERRORS
+} from '../types';
+
+const AuthState = props => {
+  const initalState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    error: null,
+    user: null
+  };
+  const [state, dispatch] = useReducer(authReducer, initalState);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthState;
